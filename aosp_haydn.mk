942409ad2901b23919f3337d697f847214149792
INCENDIOCODM <vinayakgamemaster42@gmail.com>
2023-08-27 21:30:27 +0530

    haydn: elixir-ify

--- aosp_haydn.mk
+++ aosp_haydn.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2023 The Project Elixir
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,18 +12,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from haydn device
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Elixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := haydn
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11G
-PRODUCT_NAME := lineage_haydn
+PRODUCT_NAME := aosp_haydn
 TARGET_CAMERA_USES_NEWER_HIDL_OVERRIDE_FORMAT := true
 
 PRODUCT_SYSTEM_NAME := haydn_global
 PRODUCT_SYSTEM_DEVICE := haydn
+
+# Elixir-flags
+ELIXIR_MAINTAINER := Vinayak
+ELIXIR_BUILD_TYPE := OFFICIAL
+IS_PHONE := true
+ELIXIR_BUILD_TYPE := OFFICIAL
+TARGET_INCLUDE_STOCK_ACORE := false
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="haydn_global-user 13 TKQ1.220829.002 V14.0.6.0.TKKMIXM release-keys" \
@@ -32,3 +42,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := Xiaomi/haydn_global/haydn:13/TKQ1.220829.002/V14.0.6.0.TKKMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
