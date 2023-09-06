ba6b1a252166a1b000db822a30ddbcd9ab02bbb0
INCENDIOCODM <vinayakgamemaster42@gmail.com>
2023-09-06 22:39:27 +0530

    haydn: Add vendorsetup.sh & remove.txt for CLI based builds

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,38 @@
 echo 'Starting to clone stuffs needed to build for Haydn'
 
+# Device common
+echo 'Cloning Common device tree'
+rm -rf device/xiaomi/sm8350-common
+git clone --depth=1 https://github.com/INCENDIOCODM/android_device_xiaomi_sm8350-common -b Elixir-13 device/xiaomi/sm8350-common
+
+# Kernel
+echo 'Cloning Kernel tree'
+rm -rf kernel/xiaomi/sm8350
+git clone --depth=1 https://github.com/INCENDIOCODM/android_kernel_xiaomi_sm8350 -b Elixir-13 kernel/xiaomi/sm8350
+
+# Vendor
+echo 'Cloning Vendor tree'
+rm -rf vendor/xiaomi/haydn
+git clone --depth=1 https://github.com/INCENDIOCODM/proprietary_vendor_xiaomi_haydn -b lineage-20 vendor/xiaomi/haydn
+
+# Vendor common
+echo 'Cloning Common vendor tree'
+rm -rf vendor/xiaomi/sm8350-common
+git clone --depth=1 https://github.com/INCENDIOCODM/proprietary_vendor_xiaomi_sm8350-common -b lineage-20 vendor/xiaomi/sm8350-common
+
 # Firmware
-echo 'Cloning firmware'
-git clone https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
+echo 'Cloning Firmware'
+rm -rf vendor/xiaomi/haydn-firmware
+git clone --depth=1 https://gitlab.com/Alucard_Storm/vendor_xiaomi_haydn-firmware -b thirteen vendor/xiaomi/haydn-firmware
+
+# Xiaomi
+echo 'Cloning Hardware xiaomi'
+rm -rf hardware/xiaomi
+git clone --depth=1 https://github.com/LineageOS/android_hardware_xiaomi -b lineage-20 hardware/xiaomi
 
 # Camera
-echo 'Cloning Leica Camera'
-git clone https://gitlab.com/Alucard_Storm/haydn-miuicamera -b thirteen-leica vendor/xiaomi/haydn-miuicamera
-
-echo 'Adding slow-mo fix for Leica Camera'
-cd hardware/interfaces
-wget https://github.com/Astridxx/android_hardware_interfaces/commit/ca6551863fb82110688452981572d44a26806681.patch
-patch -p1 <ca6551863fb82110688452981572d44a26806681.patch
-cd ../..
+echo 'Cloning Leica camera'
+rm -rf vendor/xiaomi/haydn-miuicamera
+git clone --depth=1 https://gitlab.com/Alucard_Storm/haydn-miuicamera -b thirteen-leica vendor/xiaomi/haydn-miuicamera
 
 echo 'delete vendorsetup.sh from device tree once this is done'
